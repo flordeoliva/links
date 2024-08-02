ad6ed2fa1d08b2dcad85220465b673f045db1e9a
itsrauldias <39931516+itsrauldias@users.noreply.github.com>
2024-08-02 13:37:11 -0400

    Updates

--- js/script.js
+++ js/script.js
@@ -1,3 +1,10 @@
-function clickFunction() {
-    alert('Click!')
+function linkToInstagram() {
+    window.open("https://www.instagram.com/flordeoliva_boutique", "_blank");
+}
+
+function linkToWhatsapp() {
+    const mensagem = 'Olá, gostaria de saber mais sobre os produtos!';
+    const fone = '65996493682';
+
+    window.open(`https://api.whatsapp.com/send?phone=${fone}&text=${mensagem}`, "_blank");
 }
